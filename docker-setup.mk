433da8bfa883b5406bc94a14da04757af5a2074c
Ralf Dragon <hypnotoad@lindra.de>
2026-04-09 09:00:37 +0200

    Allow to build hslz

--- docker-setup.mk
+++ docker-setup.mk
@@ -1,5 +1,7 @@
 user = $(shell id --user):$(shell id --group)
 hslfile = $(number)_$(name).hsl
+hslzfile = $(number)_$(name).hslz
+helpfile = doc/log$(number).html
 
 
 python = docker run --user ${user} --volume "$(shell pwd):/project" --workdir /project lindra.de/gira_hsl3 /usr/bin/python3
@@ -7,13 +9,18 @@ python = docker run --user ${user} --volume "$(shell pwd):/project" --workdir /p
 
 .PHONY: clean default
 
+ifdef BUILD_HSLZ
+default: $(hslzfile)
+else
 default: $(hslfile)
+endif
 
 $(hslfile): config.json *.py
 	$(python) /hsl/generator.pyc --source config.json --target "$@" --debug
 
-$(hslfile).zip: $(hslfile)
-	zip "$@" "$^"
+$(hslzfile): $(hslfile) $(helpfile)
+	zip --filesync --recurse-paths $@ $(hslfile) $(number)
+	zip --junk-paths $@ $(helpfile)
 
 clean:
 	rm -f "$(hslfile)"
